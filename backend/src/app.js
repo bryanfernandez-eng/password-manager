c121f31f9cb4039e78dabfe1712daed9b8bb5bb2
Bryan A Fernandez <b305ryan123@gmail.com>
2025-01-17 18:25:42 -0500

    Connectecd MongoDb and created User model

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import authRouter from "./routes/auth.route.js";
+import connectDB from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,4 +10,5 @@ app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running: http://localhost:${PORT}`);
+  connectDB(); 
 });
