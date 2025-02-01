504fadc10008ce309391fcf541b4d1ddfd07ae92
Bryan A Fernandez <b305ryan123@gmail.com>
2025-01-31 20:51:19 -0500

    Added: backend endpoints for add password and delete passwords

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import "dotenv/config";
 import authRouter from "./routes/auth.route.js";
+import passwordRouter from "./routes/password.route.js";
+
 import connectDB from "./lib/db.js";
 import cookieParser from "cookie-parser"
 
@@ -10,6 +12,7 @@ app.use(cookieParser());
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRouter);
+app.use("/api/password", passwordRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running: http://localhost:${PORT}`);
