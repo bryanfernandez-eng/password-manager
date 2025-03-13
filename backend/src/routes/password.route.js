7243c13189ab48ad76a6ae01d4298c67afccb954
Bryan A Fernandez <b305ryan123@gmail.com>
2025-03-13 15:01:59 -0400

    Added getPassword Route

--- backend/src/routes/password.route.js
+++ backend/src/routes/password.route.js
@@ -9,12 +9,17 @@ import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
+
+// POST http://localhost:3000/api/password/add
 router.post("/add", protectRoute, addPassword);
 
+// DELETE http://localhost:3000/api/password/
 router.delete("/", protectRoute, deletePassword);
 
+// DELETE http://localhost:3000/api/password/all
 router.delete("/all", protectRoute, deleteAllPasswords);
 
+// DELETE http://localhost:3000/api/password/:siteName/:email
 router.get("/:siteName/:email", protectRoute, getPassword); 
 
 
