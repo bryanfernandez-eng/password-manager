dc5d5931393d8e48fb55ae10d5c0cc65ab67c024
Bryan A Fernandez <b305ryan123@gmail.com>
2025-04-02 12:07:08 -0400

    Add Footer to overall Layout

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -16,6 +16,8 @@ import GeneratePassword from "./pages/GeneratePassword";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import ResetPassword from "./pages/auth/ResetPassword";
 import ChatbotWidget from "./components/ChatBot/ChatBotWidget";
+import { Box, Flex } from "@chakra-ui/react";
+import Footer from "./components/Footer";
 
 function App() {
   const location = useLocation();
@@ -24,25 +26,31 @@ function App() {
   return (
     <UserProvider>
       <ChakraProvider theme={theme}>
-        <NavBar />
-        <ChatbotWidget/>
-        <Routes>
-          
-          <Route path="/" element={<HomePage />} />
-          <Route path="/generate-password" element={<GeneratePassword/>}/>
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />    
-          <Route element={<PublicRoute />}>
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Route>
-          <Route element={<ProtectedRoute />}>
-            <Route path="/my-passwords" element={<PasswordPage />} />
-            <Route path="/logout" element={<Logout />} />
-          </Route>
-          <Route path="/temp" element={<TempPage />} /> {/* Deleting Later */}
-          <Route path="/*" element={<NotFoundPage />} />
-        </Routes>
+        <Flex direction={"column"} minHeight={"100vh"}>
+          <NavBar />
+          <ChatbotWidget />
+          <Box flex={1}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/generate-password" element={<GeneratePassword />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/reset-password" element={<ResetPassword />} />
+              <Route element={<PublicRoute />}>
+                <Route path="/signup" element={<SignupPage />} />
+                <Route path="/login" element={<LoginPage />} />
+              </Route>
+              <Route element={<ProtectedRoute />}>
+                <Route path="/my-passwords" element={<PasswordPage />} />
+                <Route path="/logout" element={<Logout />} />
+              </Route>
+              <Route path="/temp" element={<TempPage />} />{" "}
+              {/* Deleting Later */}
+              <Route path="/*" element={<NotFoundPage />} />
+            </Routes>
+          </Box>
+
+          <Footer />
+        </Flex>
       </ChakraProvider>
     </UserProvider>
   );
